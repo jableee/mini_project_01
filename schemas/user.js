1e5d725ca8186b27bc01b6a89114facaf6247db1
HyunjiKim818 <kjhxxxx@gmail.com>
2022-02-12 14:24:54 +0900

    user스키마

---
+++ schemas/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema ({
+    userId: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+
+    pw: {
+        tyep: String,
+        required: true,
+    },
+
+    nickname: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('User', UserSchema);
