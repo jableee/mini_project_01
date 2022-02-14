3cf535e2ac62169e20f462c374cdbb49a052e6a6
HyunjiKim818 <kjhxxxx@gmail.com>
2022-02-14 19:07:36 +0900

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
 
 const connect = require('./schemas');
 connect ();
@@ -8,3 +9,10 @@ connect ();
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extened: false }));
 
+const user = require('./routers/user');
+
+app.use('/api', user);
+
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`);
+});
