b940b9ec0d9c1837fbb81baf0bae6c58db2f7e9a
jableee <kkambo93@naver.com>
2022-02-16 01:45:28 +0900

    memo 라우터 생성 합침

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ app.use(express.urlencoded({ extened: false }));
 
 const mainRouter = require("./routers/note"); //데이터명 정해주기
 const user = require('./routers/user');
+const memoRouter = require('./routers/memo')
 app.use('/api', mainRouter);
 app.use('/api', user);
+app.use('/api', memoRouter);
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`);
--- routers/memo.js
+++ routers/memo.js
@@ -1,68 +1,93 @@
-const express = require('express')
-const router = express.Router() //라우터라고 선언한다.
-const url = require('url')
-const Memo = require('../schemas/memos')
-const authMiddleware = require('../../mini_project_01-nk/middlewares/auth-middleware.js')
+const express = require("express");
+const router = express.Router(); //라우터라고 선언한다.
+const url = require("url");
+const Memo = require("../schemas/memos");
+const authMiddleware = require("../middlewares/auth-middleware");
 
-router.get("/memo/:note_id", async (req, res) => {
-    try {
-      const { note_id } = req.params;
-      const memos = await Memo.find({
-        note_id: note_id,
-      }).sort("-date");
-      res.json({ memos: memos });
-    } catch (error) {
-      res.status(400).send({ error: error.message });
-    }
-  });
+// router.get("/memo/:note_id/", async (req, res) => {
+//   try {
+//     const { note_id } = req.params;
+//     const memos = await Memo.find({
+//       note_id: note_id,
+//     }).sort("-date");
+//     res.json({ memos: memos });
+//   } catch (error) {
+    
+//     res.status(400).send({ error: error.message });
+//   }
+// });
+
+router.get("/memo", async (req, res) => {
+  try {
+    const { memo_id } = req.body;
+    console.log(memo_id);
+    console.log(typeof(memo_id))
+    // const memos = await Memo.find({
+    //   note_id: note_id,
+    // }).sort("-date");
+    const getmemo = await Memo.findOne({  memo_id: memo_id });
+
+    res.json({ getmemo });
+  } catch (error) {
+    console.log(error)
+    res.status(400).send({ error: error.message });
+  }
+});
 
 router.post("/memo", async (req, res) => {
-    const { memo_id, note_id, memo_content, memo_title } = req.body;
-    const memos = await Memo.find({ note_id });
-    const date = new Date(+new Date() + 3240 * 10000).toISOString().replace("T", " ").replace(/\..*/, '');
-    // if (memos.length) {
-	// 	return res
-	// 	  .status(400)
-	// 	  .json({ success: true, errorMessage: "이미 있는 데이터입니다. " });
-	//   }
-	  const createdMemo = await Memo.create({ 
-        memo_id, 
-        note_id, 
-        memo_content, 
-        memo_title, 
-        date
-		})
+  const { memo_id, note_id, memo_content, memo_title } = req.body;
+  const memos = await Memo.find({ note_id });
+  const date = new Date(+new Date() + 3240 * 10000)
+    .toISOString()
+    .replace("T", " ")
+    .replace(/\..*/, "");
+  // if (memos.length) {
+  // 	return res
+  // 	  .status(400)
+  // 	  .json({ success: true, errorMessage: "이미 있는 데이터입니다. " });
+  //   }
+  const createdMemo = await Memo.create({
+    memo_id,
+    note_id,
+    memo_content,
+    memo_title,
+    date,
+  });
 
-    res.send({ memos: createdMemo });
+  res.send({ memos: createdMemo });
 });
 
 router.put("/notelist/:note_id/memo/:memo_id", async (req, res) => {
-    const { note_id, memo_id } = req.params;
-    const { memo_content,memo_title,date } = req.body;
-    
-   
-	const exist_memo = await Memo.find({ note_id: note_id, memo_id: memo_id });
-    // console.log(exist_memo)
-    if (!exist_memo.length) {
-        await Memo.create({ note_id:note_id, memo_id: memo_id, memo_content, memo_title, date });
-	}else {	
-		await Memo.updateOne({ note_id:note_id, memo_id: memo_id }, { $set: { memo_content, memo_title, date } });
-    }
-    res.send({ success: true });
+  const { note_id, memo_id } = req.params;
+  const { memo_content, memo_title, date } = req.body;
+
+  const exist_memo = await Memo.find({ note_id: note_id, memo_id: memo_id });
+  // console.log(exist_memo)
+  if (!exist_memo.length) {
+    await Memo.create({
+      note_id: note_id,
+      memo_id: memo_id,
+      memo_content,
+      memo_title,
+      date,
+    });
+  } else {
+    await Memo.updateOne(
+      { note_id: note_id, memo_id: memo_id },
+      { $set: { memo_content, memo_title, date } }
+    );
+  }
+  res.send({ success: true });
 });
 
-
 router.delete("/notelist/:note_id/memo/:memo_id", async (req, res) => {
-    const { note_id, memo_id } = req.params
+  const { note_id, memo_id } = req.params;
 
-    const deleteMemo = await Memo.find({ note_id:note_id, memo_id: memo_id });
-    if (deleteMemo.length) {
-        await Memo.deleteOne({ note_id:note_id, memo_id: memo_id });
-    }
-    res.send({ success: true });
+  const deleteMemo = await Memo.find({ note_id: note_id, memo_id: memo_id });
+  if (deleteMemo.length) {
+    await Memo.deleteOne({ note_id: note_id, memo_id: memo_id });
+  }
+  res.send({ success: true });
 });
 
 module.exports = router;
-
-
-
