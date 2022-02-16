5f06ad6742b8f97079f9cd587f465bd4de8a248d
HyunjiKim818 <kjhxxxx@gmail.com>
2022-02-16 23:50:14 +0900

    mongodb editing

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    mongoose.connect('mongodb://localhost:27017/note-memo', {
+    mongoose.connect('mongodb://localhost:27017/admin', {
         // useNewUrlParser: true,
         // useUnifiedTopology: true,
         // ignoreUndefined: true
