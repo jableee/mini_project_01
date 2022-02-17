608be593b81d19a8303620de7007bd6dd8a5ee56
nklee6300 <nogi3001@gmail.com>
2022-02-17 14:03:52 +0900

    note 스키마 수정

--- schemas/notes.js
+++ schemas/notes.js
@@ -10,7 +10,7 @@ const noteSchemas = new mongoose.Schema({
         type: String,
         required: true,
     },
-    userId: {
+    user_id: {
         type: String,
     },
     // note_image: {
